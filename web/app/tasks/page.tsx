262249155373e22faa1972490a5bcb36871c5236
TylerWardUOM <tyler.ward@student.manchester.ac.uk>
2025-03-05 23:44:14 +0000

    Simplified Version

--- web/app/tasks/page.tsx
+++ web/app/tasks/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import SubtaskInput from "../../components/ui/SubtaskInput";
 import Modal from "../../components/ui/Modal";
 import TaskModal from "../../components/ui/TaskModal";
-import { PencilIcon, CheckCircleIcon, XCircleIcon, EyeIcon, PlusCircleIcon } from '@heroicons/react/solid'; // Import Heroicons
+import { PencilIcon, CheckCircleIcon, XCircleIcon, EyeIcon, PlusCircleIcon } from '@heroicons/react/solid';
 
 const TaskListPage = () => {
   const router = useRouter();
@@ -20,29 +19,6 @@ const TaskListPage = () => {
       repeatTask: "Weekly",
       category: "Other",
       isCompleted: false,
-      subtasks: [
-        {
-          id: 1,
-          title: "Subtask 1",
-          description: "yo",
-          priority: 1,
-          duration: 25,
-          importance: 6,
-          dependency: "",
-          isCompleted: false,
-        },
-        {
-          id: 2,
-          title: "Subtask 2",
-          description: "yo",
-          priority: 4,
-          duration: 40,
-          importance: 8,
-          dependency: "",
-          isCompleted: false,
-        },
-      ],
-      isExpanded: false,
     },
     {
       id: 2,
@@ -52,57 +28,25 @@ const TaskListPage = () => {
       duration: 0.5,
       category: "Work",
       isCompleted: false,
-      subtasks: [],
-      isExpanded: false,
     },
   ]);
 
-  const [isSubtaskModalOpen, setIsSubtaskModalOpen] = useState(false);
   const [isTaskModalOpen, setIsTaskModalOpen] = useState(false);
-  const [selectedTaskId, setSelectedTaskId] = useState<number | null>(null);
   const [selectedTask, setSelectedTask] = useState<any>(null);
+  const [isPriorityGradient, setIsPriorityGradient] = useState(true);
 
-  const toggleExpand = (taskId: number) => {
-    setTasks(tasks.map((task) =>
-      task.id === taskId ? { ...task, isExpanded: !task.isExpanded } : task
-    ));
-  };
+  const [workSessionTasks, setWorkSessionTasks] = useState<any[]>([]); // Temporary work session tasks
+  const [isWorkSessionCreated, setIsWorkSessionCreated] = useState(false); // To track if Work Session has been created
 
-  const addSubtask = (newSubtask: any) => {
-    if (selectedTaskId !== null) {
-      setTasks(tasks.map((task) =>
-        task.id === selectedTaskId ? {
-          ...task,
-          subtasks: [...task.subtasks, newSubtask],
-        } : task
-      ));
-    }
+  const openNewTaskModal = () => {
+    setSelectedTask(null); // Reset selected task for new task creation
+    setIsTaskModalOpen(true);
   };
 
   const markTaskAsComplete = (taskId: number) => {
     setTasks(tasks.map((task) =>
       task.id === taskId ? { ...task, isCompleted: !task.isCompleted } : task
     ));
-  };
-
-  const markSubtaskAsComplete = (taskId: number, subtaskId: number) => {
-    setTasks(tasks.map((task) =>
-      task.id === taskId
-        ? {
-            ...task,
-            subtasks: task.subtasks.map((subtask) =>
-              subtask.id === subtaskId
-                ? { ...subtask, isCompleted: !subtask.isCompleted }
-                : subtask
-            ),
-          }
-        : task
-    ));
-  };
-
-  const openSubtaskModal = (taskId: number) => {
-    setSelectedTaskId(taskId);
-    setIsSubtaskModalOpen(true);
   };
 
   const openTaskModal = (taskId: number) => {
@@ -113,28 +57,20 @@ const TaskListPage = () => {
     }
   };
 
-  const closeSubtaskModal = () => {
-    setIsSubtaskModalOpen(false);
-    setSelectedTaskId(null);
-  };
-
   const closeTaskModal = () => {
     setIsTaskModalOpen(false);
     setSelectedTask(null);
   };
 
-  const getNextSubtaskId = (taskId: number): number => {
-    const task = tasks.find((task) => task.id === taskId);
-    if (task && task.subtasks.length > 0) {
-      return Math.max(...task.subtasks.map((subtask) => subtask.id)) + 1;
-    }
-    return 1; 
-  };
-
   const handleSaveTask = (updatedTask: any) => {
-    setTasks(tasks.map((task) =>
-      task.id === updatedTask.id ? updatedTask : task
-    ));
+    if (selectedTask) {
+      setTasks(tasks.map((task) =>
+        task.id === updatedTask.id ? updatedTask : task
+      ));
+    } else {
+      const newTask = { ...updatedTask, id: tasks.length + 1 };
+      setTasks([...tasks, newTask]);
+    }
     closeTaskModal();
   };
 
@@ -142,16 +78,39 @@ const TaskListPage = () => {
     setTasks(tasks.filter((task) => task.id !== taskId));
   };
 
-  const deleteSubtask = (taskId: number, subtaskId: number) => {
-    setTasks(tasks.map((task) =>
-      task.id === taskId
-        ? { ...task, subtasks: task.subtasks.filter((subtask) => subtask.id !== subtaskId) }
-        : task
-    ));
+  const goToFocusMode = (taskId: number) => {
+    router.push(`/focus?task=${taskId}`);
   };
 
-  const goToFocusMode = (taskId: number, subtaskId?: number) => {
-    router.push(`/focus?task=${taskId}${subtaskId ? `&subtask=${subtaskId}` : ""}`);
+  const getPriorityColor = (priority: number) => {
+    if (isPriorityGradient) {
+      switch (priority) {
+        case 1:
+          return "bg-green-200"; // Low priority - green
+        case 2:
+          return "bg-yellow-300"; // Medium priority - yellow
+        case 3:
+          return "bg-red-400"; // High priority - red
+        default:
+          return "bg-gray-300"; // Default
+      }
+    }
+    return ""; // No gradient if disabled
+  };
+
+  const addToWorkSession = (task: any) => {
+    // Adds the task to the work session list
+    setWorkSessionTasks((prevTasks) => [...prevTasks, task]);
+  };
+
+  const removeFromWorkSession = (taskId: number) => {
+    // Removes the task from the work session list
+    setWorkSessionTasks(workSessionTasks.filter((task) => task.id !== taskId));
+  };
+
+  const clearWorkSession = () => {
+    setWorkSessionTasks([]); // Clears all tasks in work session
+    setIsWorkSessionCreated(false); // Reset work session creation state
   };
 
   return (
@@ -161,10 +120,69 @@ const TaskListPage = () => {
           <button className="bg-gray-200 text-gray-700 px-4 py-2 rounded">Filter</button>
           <button className="bg-gray-200 text-gray-700 px-4 py-2 rounded">Sort by</button>
           <button className="bg-gray-200 text-gray-700 px-4 py-2 rounded">Group by Category/Tag</button>
+          <div className="flex items-center space-x-2">
+            <label className="text-sm">Priority Gradient:</label>
+            <input
+              type="checkbox"
+              checked={isPriorityGradient}
+              onChange={() => setIsPriorityGradient(!isPriorityGradient)}
+              className="cursor-pointer"
+            />
+          </div>
         </div>
+        <button
+          onClick={openNewTaskModal}
+          className="bg-blue-500 text-white px-4 py-2 rounded flex items-center space-x-2"
+        >
+          <PlusCircleIcon className="h-5 w-5" />
+          <span>New Task</span>
+        </button>
       </div>
 
-      <div className="overflow-x-auto bg-white shadow-md rounded-lg">
+      {/* Work Session Box */}
+      {!isWorkSessionCreated ? (
+        <button
+          onClick={() => setIsWorkSessionCreated(true)}
+          className="bg-green-500 text-white px-4 py-2 rounded mt-4 flex items-center space-x-2"
+        >
+          <PlusCircleIcon className="h-5 w-5" />
+          <span>Create Work Session</span>
+        </button>
+      ) : (
+        <div className="bg-gray-100 p-4 rounded-lg mt-4">
+          <h3 className="text-xl font-semibold">Work Session</h3>
+          <button
+            onClick={clearWorkSession}
+            className="bg-red-500 text-white px-4 py-2 rounded mt-4"
+          >
+            Clear Session
+          </button>
+          <div className="mt-4">
+            {workSessionTasks.length > 0 ? (
+              <ul className="list-none">
+                {workSessionTasks.map((task, index) => (
+                  <li key={task.id} className="p-2 my-2 bg-gray-200 rounded">
+                    <div className="flex justify-between items-center">
+                      <span>{index + 1}. {task.title}</span>
+                      <button
+                        onClick={() => removeFromWorkSession(task.id)}
+                        className="bg-red-500 text-white px-2 py-1 rounded"
+                      >
+                        <XCircleIcon className="h-5 w-5" />
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No tasks in the current session.</p>
+            )}
+          </div>
+        </div>
+      )}
+
+      {/* Task Table */}
+      <div className="overflow-x-auto bg-white shadow-md rounded-lg mt-4">
         <table className="min-w-full">
           <thead className="bg-gray-100">
             <tr>
@@ -178,138 +196,70 @@ const TaskListPage = () => {
           </thead>
           <tbody>
             {tasks.map((task) => (
-              <>
-                <tr key={task.id}>
-                  <td className="py-2 px-4 flex items-center">
-                    <span className={task.isCompleted ? "line-through text-gray-400" : ""}>
-                      {task.title}
-                    </span>
-                    <span className="ml-2 text-sm text-gray-500">({task.subtasks.length} subtasks)</span>
-                  </td>
-                  <td className="py-2 px-4">
-                    <div className="flex items-center">
-                      {[...Array(task.priority)].map((_, index) => (
-                        <span key={index} className="text-yellow-500">★</span>
-                      ))}
-                    </div>
-                  </td>
-                  <td className="py-2 px-4">{task.dueDate}</td>
-                  <td className="py-2 px-4">{task.category}</td>
-                  <td className="py-2 px-4">{task.duration} hours</td>
-                  <td className="py-2 px-4 flex space-x-2">
-                    <button
-                      onClick={() => toggleExpand(task.id)}
-                      className="bg-blue-500 text-white px-4 py-2 rounded"
-                    >
-                      <EyeIcon className="h-5 w-5" />
-                    </button>
-                    <button
-                      onClick={() => openTaskModal(task.id)} 
-                      className="bg-green-500 text-white px-4 py-2 rounded"
-                    >
-                      <PencilIcon className="h-5 w-5" />
-                    </button>
-                    <button
-                      onClick={() => markTaskAsComplete(task.id)}
-                      className={`px-4 py-2 rounded ${task.isCompleted ? "bg-red-500" : "bg-yellow-500"}`}
-                    >
-                      {task.isCompleted ? (
-                        <XCircleIcon className="h-5 w-5" />
-                      ) : (
-                        <CheckCircleIcon className="h-5 w-5" />
-                      )}
-                    </button>
-                    <button
-                      onClick={() => openSubtaskModal(task.id)}
-                      className="bg-indigo-500 text-white px-4 py-2 rounded"
-                    >
-                      <PlusCircleIcon className="h-5 w-5" />
-                    </button>
-                    <button
-                      onClick={() => deleteTask(task.id)}
-                      className="bg-red-500 text-white px-4 py-2 rounded"
-                    >
+              <tr key={task.id} className={`${getPriorityColor(task.priority)} ${task.isCompleted ? 'opacity-50' : ''}`}>
+                <td className="py-2 px-4">
+                  <span className={task.isCompleted ? "line-through text-gray-400" : ""}>
+                    {task.title}
+                  </span>
+                </td>
+                <td className="py-2 px-4">
+                  <div className="flex items-center">
+                    {[...Array(task.priority)].map((_, index) => (
+                      <span key={index} className="text-yellow-500">★</span>
+                    ))}
+                  </div>
+                </td>
+                <td className="py-2 px-4">{task.dueDate}</td>
+                <td className="py-2 px-4">{task.category}</td>
+                <td className="py-2 px-4">{task.duration} hours</td>
+                <td className="py-2 px-4 flex space-x-2">
+                  <button
+                    onClick={() => openTaskModal(task.id)}
+                    className="bg-green-500 text-white px-4 py-2 rounded"
+                  >
+                    <PencilIcon className="h-5 w-5" />
+                  </button>
+                  <button
+                    onClick={() => markTaskAsComplete(task.id)}
+                    className={`px-4 py-2 rounded ${task.isCompleted ? "bg-red-500" : "bg-yellow-500"}`}
+                  >
+                    {task.isCompleted ? (
                       <XCircleIcon className="h-5 w-5" />
-                    </button>
-                    <button
-                        onClick={() => goToFocusMode(task.id)}
-                        className="bg-purple-500 text-white px-4 py-2 rounded flex items-center space-x-2"
-                        >
-                        <EyeIcon className="h-5 w-5" />
-                    </button>
-                  </td>
-                </tr>
-
-                {task.isExpanded && task.subtasks.map((subtask) => (
-                  <tr key={subtask.id}>
-                    <td className="py-2 px-4 pl-12">
-                      <span className={subtask.isCompleted ? "line-through text-gray-400" : ""}>
-                        {subtask.title}
-                      </span>
-                    </td>
-                    <td className="py-2 px-4">
-                      <div className="flex items-center">
-                        {[...Array(subtask.priority)].map((_, index) => (
-                          <span key={index} className="text-yellow-500">★</span>
-                        ))}
-                      </div>
-                    </td>
-                    <td className="py-2 px-4">-</td>
-                    <td className="py-2 px-4">-</td>
-                    <td className="py-2 px-4">{subtask.duration} minutes</td>
-                    <td className="py-2 px-4 flex space-x-2">
-                      {/* Edit Subtask Button (Non-functional for now) */}
-                      <button className="bg-green-500 text-white px-4 py-2 rounded">
-                        <PencilIcon className="h-5 w-5" />
-                      </button>
-
-                      {/* Complete/Incomplete Toggle Button */}
-                      <button
-                        onClick={() => markSubtaskAsComplete(task.id, subtask.id)}
-                        className={`px-4 py-2 rounded ${subtask.isCompleted ? "bg-red-500" : "bg-yellow-500"}`}
-                      >
-                        {subtask.isCompleted ? (
-                          <XCircleIcon className="h-5 w-5" />
-                        ) : (
-                          <CheckCircleIcon className="h-5 w-5" />
-                        )}
-                      </button>
-
-                      {/* Delete Subtask Button */}
-                      <button
-                        onClick={() => deleteSubtask(task.id, subtask.id)}
-                        className="bg-red-500 text-white px-4 py-2 rounded"
-                      >
-                        <XCircleIcon className="h-5 w-5" />
-                      </button>
-                      <button
-                        onClick={() => goToFocusMode(task.id, subtask.id)}
-                        className="bg-purple-500 text-white px-4 py-2 rounded flex items-center space-x-2"
-                        >
-                        <EyeIcon className="h-5 w-5" />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </>
+                    ) : (
+                      <CheckCircleIcon className="h-5 w-5" />
+                    )}
+                  </button>
+                  <button
+                    onClick={() => deleteTask(task.id)}
+                    className="bg-red-500 text-white px-4 py-2 rounded"
+                  >
+                    <XCircleIcon className="h-5 w-5" />
+                  </button>
+                  <button
+                    onClick={() => goToFocusMode(task.id)}
+                    className="bg-purple-500 text-white px-4 py-2 rounded flex items-center space-x-2"
+                  >
+                    <EyeIcon className="h-5 w-5" />
+                  </button>
+                  <button
+                    onClick={() => addToWorkSession(task)} // Adds task to session
+                    className="bg-blue-500 text-white px-4 py-2 rounded"
+                  >
+                    Add to Session
+                  </button>
+                </td>
+              </tr>
             ))}
           </tbody>
         </table>
       </div>
 
-      <Modal isOpen={isSubtaskModalOpen} onClose={closeSubtaskModal} width="max-w-[20%]">
-        <h2 className="text-xl font-semibold mb-4">Add Subtask</h2>
-        <SubtaskInput
-          onAddSubtask={addSubtask}
-          nextId={getNextSubtaskId(selectedTaskId as number)}
-        />
-      </Modal>
-
-      <Modal isOpen={isTaskModalOpen} onClose={closeTaskModal} width="max-w-[20%]">
+      {/* Task Modal */}
+      <Modal isOpen={isTaskModalOpen} onClose={closeTaskModal} width="max-w-3xl">
         <TaskModal
-          onClose={closeTaskModal}
+          task={selectedTask}
           onSave={handleSaveTask}
-          existingTask={selectedTask}
+          onCancel={closeTaskModal}
         />
       </Modal>
     </div>
